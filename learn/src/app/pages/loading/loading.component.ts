f60e62daf7d30a01f007815c42ce1c3b3e801555
jgduckie <josegabrielduckie@outlook.com>
2024-02-14 23:29:54 +0100

    add boostrap

--- learn/src/app/pages/loading/loading.component.ts
+++ learn/src/app/pages/loading/loading.component.ts
@@ -5,7 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.css'
 })
-export class LoadingComponent{
+export class LoadingComponent implements OnInit {
+  ngOnInit(): void {
+    this.runStyle()
+  }
+  style: string = "width: %";
+  numero = 0;
+
+  runStyle() {
+    while (this.numero != 100) {
+        console.log(this.style)
+        this.style = "width:" + this.numero + "%;"
+        this.numero++;
+
+    }
+
+
+  }
 
 }
 
